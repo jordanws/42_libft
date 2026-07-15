777b6f0516a6335e8895df027ffab14a3ac5dc00
Jowi <93743976+jordanws@users.noreply.github.com>
2026-07-15 09:18:51 -0300

    feat(ft_memcpy.c): minor corrections

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -9,11 +9,11 @@ void    *ft_memcpy(void *dest, const void *src, size_t n)
     if (!dest && !src)
         return (NULL);
     d = (unsigned char *)dest;
-    s = (const unsigned char *)src;
+    s = (unsigned char *)src;
     i = 0;
     while (i < n)
     {
-        d[i] = s[i]
+        d[i] = s[i];
         i++;
     }
     return (dest);
